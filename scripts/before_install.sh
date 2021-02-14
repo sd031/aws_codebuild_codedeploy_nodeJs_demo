aa7fb030c20a338556d86c0911a2479ea149cf44
Sandip Das <sandip@gryphonconnect.com>
2021-02-14 23:46:43 +0530

    script fixes

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+FILE=/home/ubuntu/app/index.js
+if test -f "$FILE"; then
+   echo "$FILE exists"
+   cd /home/ubuntu/
+   pm2 stop --silent index.js
+else 
+    echo "$FILE does not exist."
+fi
 cd /home/ubuntu/
 sudo rm -rf app
 sudo mkdir app
