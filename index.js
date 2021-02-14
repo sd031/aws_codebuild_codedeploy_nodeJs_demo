6ddb4e1034c762bd3c0cc514e5bc27a4df4725d7
Sandip Das <sandip@gryphonconnect.com>
2021-02-14 22:42:37 +0530

    v2 deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('CodeDeploy Sample V1!')
+  res.send('CodeDeploy Sample V2!')
 })
 
 app.get('/status', (req, res) => {
