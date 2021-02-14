bb0cce4364ef3328252530b0cbb9be30479f5b75
Sandip Das <sandip@gryphonconnect.com>
2021-02-14 23:12:05 +0530

    v5 deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('CodeDeploy Sample V3!')
+  res.send('CodeDeploy Sample V5!')
 })
 
 app.get('/status', (req, res) => {
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/app
-pm2 start index.js
+pm2 restart index.js
