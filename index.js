a389aa0cfabbcc5c65d25db3913bbb011e2817c1
Sandip Das <sandip@gryphonconnect.com>
2021-02-15 00:23:41 +0530

    final project script ready

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('CodeDeploy Sample V8!')
+  res.send('CodeDeploy Sample V1!')
 })
 
 app.get('/status', (req, res) => {
