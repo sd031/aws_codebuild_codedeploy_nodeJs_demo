c7688111dfb33a5a4a82b3e055abd5f26a07540c
Sandip Das <sandip@gryphonconnect.com>
2021-02-15 00:05:21 +0530

    v8

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('CodeDeploy Sample V7!')
+  res.send('CodeDeploy Sample V8!')
 })
 
 app.get('/status', (req, res) => {
