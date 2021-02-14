1dc1ea61dba2f609d033f9b8719cc7c225c90dde
Sandip Das <sandip@gryphonconnect.com>
2021-02-14 23:21:04 +0530

    fixes

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -2,7 +2,7 @@
 FILE=/home/ubuntu/app/index.js
 if test -f "$FILE"; then
    echo "$FILE exists., so trying again"
-   cd /home/ubuntu/app
+   cd /home/ubuntu/
    pm2 stop --silent index.js
 else 
     echo "$FILE does not exist."
