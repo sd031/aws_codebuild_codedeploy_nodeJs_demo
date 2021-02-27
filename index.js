871d0c7d1b042aafef874f1303254774a3924ac8
Sandip Das <sandip@gryphonconnect.com>
2021-02-28 00:59:22 +0530

    changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CodeDeploy Sample v5!')
+  res.send('CodeDeploy Sample v1!')
 })
 
 app.get('/status', (req, res) => {
