59ff73b4e1779ce1b5bd7933a163808b7d101c36
tu1306 <tranvantu20@gmail.com>
2022-01-17 16:45:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V2!')
+  res.send('CICD App V2.1!')
 })
 
 app.get('/status', (req, res) => {
