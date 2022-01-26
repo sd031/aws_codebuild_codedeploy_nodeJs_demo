a15248e0e253fbebd41d664d85a9413910568e99
markodemic@gmail.com <markodemic@gmail.com>
2022-01-26 12:43:17 +0100

    v108 index2

--- newfolder/index2.js
+++ newfolder/index2.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V107!')
+  res.send('CICD App V108!')
 })
 
 app.get('/status', (req, res) => {
