df686a38ebf6d6bf405cbd153af640e5c99fc201
markodemic@gmail.com <markodemic@gmail.com>
2022-02-02 16:05:49 +0100

    v50

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V49')
+  res.send('CICD App V50')
 })
 
 app.get('/status', (req, res) => {
