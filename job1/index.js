7cb67afacb3169bdb3368c64506c9332898c6ab8
markodemic@gmail.com <markodemic@gmail.com>
2022-02-03 16:52:31 +0100

    Add job1

---
+++ job1/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000 ;
+const config = require('config')
+console.log(config);
+
+app.get('/', (req, res) => {
+  res.send('CICD App V68')
+})
+
+app.get('/status', (req, res) => {
+    res.send('ok')
+  })
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
