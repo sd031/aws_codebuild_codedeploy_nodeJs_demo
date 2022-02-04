2b9b8495e44e645fd6786c4ff7c55de7ecc73afd
markodemic@gmail.com <markodemic@gmail.com>
2022-02-04 17:16:49 +0100

    job1 v77

--- job1/index.js
+++ job1/index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V76')
+  res.send('CICD App V77')
 })
 
 app.get('/status', (req, res) => {
