cfcaefba7264975ceccce61125763bb6ef395c29
demask <43813282+demask@users.noreply.github.com>
2022-02-11 23:21:52 +0100

    Update index.js

--- job2/index.js
+++ job2/index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App job2 v17')
+  res.send('CICD App job2 v18')
 })
 
 app.get('/status', (req, res) => {
